663e18c625486c35613e585ea923bb80a2bbe470
Bryce <brycebrown91@hotmail.com>
2019-11-05 22:02:58 -0600

    added category_id column to product table and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_033154) do
+ActiveRecord::Schema.define(version: 2019_11_06_040117) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_033154) do
     t.string "size"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
