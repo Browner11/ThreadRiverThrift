77645f031089a7e6a183e77459032a2393591c28
Bryce <brycebrown91@hotmail.com>
2019-11-12 13:50:32 -0600

    Created many to many relationship between products and categories and restructered schema to reflect the change

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  belongs_to :category
+  has_and_belongs_to_many :category
 
   has_one_attached :image
+  paginates_per 9
+
+  def thumbnail
+    image.variant(resize: '300x300!')
+  end
+
+  def medium
+    image.variant(resize: '600x600!')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_042951) do
+ActiveRecord::Schema.define(version: 2019_11_12_194335) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -65,6 +65,11 @@ ActiveRecord::Schema.define(version: 2019_11_06_042951) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
@@ -72,7 +77,6 @@ ActiveRecord::Schema.define(version: 2019_11_06_042951) do
     t.string "size"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "category_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
