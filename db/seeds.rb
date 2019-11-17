a659fe670538953dd37ffd298d773404ab9dda16
Bryce <brycebrown91@hotmail.com>
2019-11-17 16:39:50 -0600

    added about us and contact pages to seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+Page.create(title: 'About Us', content: 'Please fill this in', permalink: 'about-us')
+Page.create(title: 'Contact', content: 'Please fill this in', permalink: 'contact')
