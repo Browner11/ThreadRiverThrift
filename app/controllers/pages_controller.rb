0b4f97b191792a4500e210cc1df4b9ab7bde844a
Bryce <brycebrown91@hotmail.com>
2019-11-17 16:01:54 -0600

    implemented the permalink action and route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < InheritedResources::Base
 
-  private
+  http_basic_authenticate_with name: "admin", password: "password"
 
+  def permalink
+    @page = Page.find_by_permalink(params[:permalink])
+    if @page
+      render :show
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
     def page_params
       params.require(:page).permit(:title, :content, :permalink)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :pages
+
+  get '/static/:permalink', to: 'pages#permalink', as: 'permalink'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'products#index'
   root to: 'products#index'
