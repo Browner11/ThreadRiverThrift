4fe29027ad2923322bcc3fb86f1768b89571d72d
Bryce <brycebrown91@hotmail.com>
2019-11-19 11:29:59 -0600

    added a search result collection route to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  # before_action :load_cart
+
+
   def index
     @products = Product.page params[:page]
   end
@@ -6,4 +9,26 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search_results
+    
+  end
+
+  # def add_to_cart
+  #   id = params[:id].to_i
+  #   session[:cart] << id unless session[:cart].include?(id)
+  #   redirect_to root_path
+  # end
+
+  # def delete_from_cart
+  #   id = params[:id].to_i
+  #   session[:cart].delete(id)
+  #   redirect_to root_path
+  # end
+
+  # private
+
+  # def load_cart
+  #   @cart = Product.find(session[:cart])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'category/index'
+  get 'category/show'
   resources :pages
 
   get '/static/:permalink', to: 'pages#permalink', as: 'permalink'
@@ -6,7 +8,16 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :products, only: %i[index show]
+  resources :products, only: %i[index show] do
+    collection do
+      get 'search_results'
+    end
+  end
+
+
+
+  # post 'cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
+  # delete 'cart/:id', to: 'products#delete_from_cart', as: 'delete_to_cart'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'products#index'
